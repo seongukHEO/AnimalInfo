c7afa18b4c110124e638fe1d23a9d93ebfdb15b8
seongukHEO <heoseonguk3@gmail.com>
2024-02-07 23:58:51 +0900

    Retry

--- app/src/main/java/kr/co/lion/android01/androidprojecttest/InputAnimalActivity.kt
+++ app/src/main/java/kr/co/lion/android01/androidprojecttest/InputAnimalActivity.kt
@@ -35,7 +35,34 @@ class InputAnimalActivity : AppCompatActivity() {
     }
 
     //뷰들의 이벤트 설정
+    //여기서 이벤트란 무언가를 클릭했을 때 보여지거나 그러는 것!
     fun setEvent(){
+        //토글버튼을 눌렀을 때 해당하는 버튼에 따라 보여지는 것!
+        activityInputAnimalBinding.apply {
+            toggleGroup1.addOnButtonCheckedListener { group, checkedId, isChecked ->
+                //모두 안보이게 한다
+                containerInputType1.isVisible = false
+                containerInpuptType2.isVisible = false
+                containerInputType3.isVisible = false
+
+                when(toggleGroup1.checkedButtonId){
+                    R.id.lionButton -> {
+                        //항목에 맞는 것들을 보이게 한다
+                        containerInputType1.isVisible = true
+                        //입력요소 초기화
+                        toggleGroup2.check(R.id.buttonGender1)
+                        textFieldInputHairCount.setText("")
+
+                    }
+                    R.id.tigerButton -> {
+                        //항목에 맞는 것을 보이게 한다
+                        containerInpuptType2.isVisible = true
+                        //입력요소 초기화 ->
+                    }
+                }
+
+            }
+        }
 
     }
 
